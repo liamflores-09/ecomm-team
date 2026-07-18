4f71597d6a831b66ee0e85325d60b86043179c47
liamflores-09 <liamjedmflores@gmail.com>
2026-07-18 11:54:23 +0800

    Update BrandSeeder.php

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -4,15 +4,12 @@ namespace Database\Seeders;
 
 use App\Models\Brand;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class BrandSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Brand::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        Brand::query()->delete();
 
         $brands = [
             ['name' => '1 SPOT',           'classification' => 'Tech',            'description' => 'Power supply solutions for pedalboards and effects'],
