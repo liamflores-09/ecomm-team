2af67cb745888152e655bbbba5f43fc425666353
liamflores-09 <liamjedmflores@gmail.com>
2026-07-18 12:08:29 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,17 +1,18 @@
 <?php
-
 namespace App\Providers;
-
 use App\View\Composers\PreviewRoleComposer;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
-
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         View::composer('*', PreviewRoleComposer::class);
     }
 }
